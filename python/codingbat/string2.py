837990d8895f46d5b78a3484be6a5a54c8ab7d27
nobasecase <rollyfuentes.dev@gmail.com>
2023-11-30 15:42:16 -0600

    added string2 problems

--- python/codingbat/string2.py
+++ python/codingbat/string2.py
@@ -53,7 +53,7 @@ def cat_dog(str):
     return cat_count == dog_count
 
 
-print(cat_dog("catdog"))
+# print(cat_dog("catdog"))
 
 """
 Return the number of times that the string "code" appears anywhere in the given string,
@@ -110,3 +110,168 @@ xyz_there('abcxyz') → True
 xyz_there('abc.xyz') → False
 xyz_there('xyz.abc') → True
 """
+
+
+def xyz_there(str):
+    if len(str) < 3:
+        return False
+    elif str == "xyz":
+        return True
+    else:
+        for i in range(len(str) - 2):
+            if str[i : i + 3] == "xyz":
+                if str[i - 1] != ".":
+                    return True
+    return False
+
+
+"""
+Return the number of even ints in the given array. 
+Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+
+count_evens([2, 1, 2, 3, 4]) → 3
+count_evens([2, 2, 0]) → 3
+count_evens([1, 3, 5]) → 0
+"""
+
+
+def count_evens(nums):
+    even_nums = 0
+    for num in nums:
+        if num % 2 == 0:
+            even_nums += 1
+    return even_nums
+
+
+"""
+Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array.
+Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
+
+
+big_diff([10, 3, 5, 6]) → 7
+big_diff([7, 2, 10, 9]) → 8
+big_diff([2, 10, 7, 2]) → 8
+"""
+
+
+def big_diff(nums):
+    if len(nums) == 1:
+        return 0
+    elif len(nums) == 2:
+        return abs(nums[0] - nums[1])
+    else:
+        min = nums[0]
+        max = nums[0]
+        for num in nums:
+            if num < min:
+                min = num
+            if num > max:
+                max = num
+        return abs(max - min)
+
+
+"""
+Return the "centered" average of an array of ints, which we'll say is the mean average of the values,
+except ignoring the largest and smallest values in the array.
+If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value.
+
+Use int division to produce the final average. You may assume that the array is length 3 or more.
+
+
+centered_average([1, 2, 3, 4, 100]) → 3
+centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
+centered_average([-10, -4, -2, -4, -2, 0]) → -3
+"""
+
+
+def centered_average(nums):
+    min = nums[0]
+    max = nums[0]
+    total = 0
+    for i in range(len(nums)):
+        if nums[i] < min:
+            min = nums[i]
+        if nums[i] > max:
+            max = nums[i]
+
+    for num in nums:
+        total = total + num
+    total = (total - (max + min)) / (len(nums) - 2)
+    return total
+
+
+"""
+Return the sum of the numbers in the array, returning 0 for an empty array.
+Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+sum13([1, 2, 2, 1]) → 6
+sum13([1, 1]) → 2
+sum13([1, 2, 2, 1, 13]) → 6
+"""
+
+
+def sum13(nums):
+    sum = 0
+    if len(nums) == 0:
+        return sum
+    index = 0
+    while index < len(nums):
+        if nums[index] == 13:
+            index = index + 2
+        else:
+            sum += nums[index]
+            index = index + 1
+    return sum
+
+
+"""
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7
+(every 6 will be followed by at least one 7).
+
+Return 0 for no numbers.
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+"""
+
+
+def sum67(nums):
+    index = 0
+    flag = False
+    sum = 0
+
+    while index < len(nums):
+        if nums[index] == 6:
+            flag = True
+        elif flag and nums[index] == 7:
+            flag = False
+        elif flag:
+            pass
+        else:
+            sum += nums[index]
+        index = index + 1
+    return sum
+
+
+"""
+Given an array of ints, return True if the array contains a 2 next to a 2 somewhere.
+
+
+has22([1, 2, 2]) → True
+has22([1, 2, 1, 2]) → False
+has22([2, 1, 2]) → False
+"""
+
+
+def has22(nums):
+    if len(nums) < 2:
+        return False
+    for i in range(len(nums)):
+        if nums[i] == 2:
+            if i == len(nums) - 1:
+                break
+            elif nums[i + 1] == 2:
+                return True
+    return False
